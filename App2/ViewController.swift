f0a6922aeb0f4eb650f3025358c20803cf4c6a41
enriquemore587 <jose.vergara2104@hotmail.com>
2019-09-27 19:08:46 -0500

    first commit

--- App2/ViewController.swift
+++ App2/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  App2
@@ -9,12 +10,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var images = ["machu.jpg", "cuba.jpeg", "cuba2.jpeg", "cuba3.jpeg", "mexico.jpg"]
+    
+    var index = 0
 
+    @IBOutlet weak var labelTexto: UILabel!
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.labelTexto.text = self.images[self.index]
+        self.labelTexto.textColor = UIColor.white
+        self.labelTexto.backgroundColor = UIColor.black
+        
+        
+        self.image.backgroundColor = UIColor.darkGray
+        self.image.image = UIImage(named: self.images[self.index])
+        self.image.contentMode = .redraw
+        
+        self.button.backgroundColor = UIColor.black
+        self.button.setTitleColor(UIColor.white, for: .normal)
+        self.button.titleLabel?.text = "Clickeame"
+        self.button.layer.borderWidth = 2
+        self.button.layer.cornerRadius = 8
     }
-
-
+    
+    
+    @IBAction func btnPresionado(_ sender: UIButton) {
+        self.labelTexto.backgroundColor = UIColor.black
+        self.labelTexto.textColor = UIColor.white
+        
+        if self.index == self.images.count-1 {
+            self.index = 0
+        } else {
+            self.index = self.index+1
+        }
+        
+        self.labelTexto.text = self.images[index]
+        
+        self.image.image = UIImage(named: self.images[index])
+        print(self.images[index])
+    }
+    
 }
 
---
+++ README.md
@@ -0,0 +1,1 @@
+# Swift App1 changing images from a Image array with a Button
